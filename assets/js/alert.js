c2ee7074ee19539f9d6f60842a0b2cf89bd9b681
Elizabeth Pincheira <elizabeth.pincheira.caro@gmail.com>
2023-11-27 14:32:43 -0300

    inicio proyecto

---
+++ assets/js/alert.js
@@ -0,0 +1,9 @@
+
+
+// alert('hola mundo');
+
+// let nombre = prompt ('¿Cuál es tu nombre');
+// console.log('***' + nombre + '***')
+
+const seleccion = confirm('¿esta seguro de borrar esto?');
+console.log(seleccion)
---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+
+
+//console.log(minombre);
+//alert ('Hola desde app.js');
+
+//console.log('Hola Mundo');
+
+let a = 10,
+    b = 20,
+    c = 'Hola ',
+    d = 'Spiderman',
+    x = a + b;
+
+const saludo = c + d;
+//console.log(saludo)
+//console.log('%c Mis variables', 'color:blue; font-weight:bold');
+//console.log({a});
+//console.log({b});
+//console.log({c});
+//console.table({a,b,c,d,x});
+
+let minombre = 'Elizabeth';
+
