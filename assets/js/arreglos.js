a6755b1e01735fad3832ae0533b156b5d852a2a0
Elizabeth Pincheira <elizabeth.pincheira.caro@gmail.com>
2023-11-28 10:15:56 -0300

    arreglos2

---
+++ assets/js/arreglos.js
@@ -0,0 +1,24 @@
+
+
+//let arr = new Array(10);
+// const arr = [];
+// console.log(arr);
+
+let videoJuegos = ['zeldaOT', 'zeldaBW','zeldaTK'];
+console.log({videoJuegos})
+
+console.log(videoJuegos[0]);
+
+let arregloCosas = [
+    true,
+    123,
+    'Elita',
+    1 + 2,
+    function () {},
+    ()=>{},
+    {a:1},
+    ['Link', 'Zelda', 'Ganondorf', 'Sidon',['Cuco', 'Rupinejo']]
+];
+
+console.log({arregloCosas});
+console.log(arregloCosas[7][4][1]);
